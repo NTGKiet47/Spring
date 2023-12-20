48a224b30f8d9434d786b8656f249a71a09536a7
Gia Kiệt <nguyentrangiakiet47@gmail.com>
2023-12-20 19:24:46 +0700

    Using Primary

--- exp4/src/main/java/com/example/main/Example4.java
+++ exp4/src/main/java/com/example/main/Example4.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Example4 {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Vehicles v = context.getBean("Mercedes",Vehicles.class);
+        Vehicles v = context.getBean("Ford",Vehicles.class);
         System.out.println(v.getName());
     }
 }
