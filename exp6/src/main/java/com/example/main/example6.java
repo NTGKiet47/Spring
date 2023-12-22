1b14a0e156bb552718c30141f168fb0a8cdea1f4
Gia Kiệt <nguyentrangiakiet47@gmail.com>
2023-12-22 17:10:42 +0700

    postConstruct, preDestroy:

--- exp6/src/main/java/com/example/main/example6.java
+++ exp6/src/main/java/com/example/main/example6.java
@@ -4,13 +4,14 @@ import com.example.beans.Vehicle;
 import com.example.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class example5 {
+public class example6 {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         Vehicle v = context.getBean(Vehicle.class);
 /*Right here the name return will be NULL
  because dev cannot control while creating the object
  So that also means why the project config has no content*/
-        System.out.println("Name: "+v.getName());
+        System.out.println("Vehicle name: "+v.getName());
+        context.close();
     }
 }
