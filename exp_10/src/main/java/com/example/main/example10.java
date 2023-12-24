00f5833de3ae467c4ba6639615ad8c7fc8947e7f
Gia Kiệt <nguyentrangiakiet47@gmail.com>
2023-12-24 18:40:21 +0700

    wiring using parameter in method

---
+++ exp_10/src/main/java/com/example/main/example10.java
@@ -0,0 +1,14 @@
+package com.example.main;
+
+import com.example.beans.*;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class example10 {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Person person = context.getBean(Person.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println(person.getName() +" owns vehicle "+person.getVehicle());
+    }
+}
