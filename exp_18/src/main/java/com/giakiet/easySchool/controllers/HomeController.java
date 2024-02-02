edb1c0fd4cbb8c394e621dd56f9853c22abf07a3
Gia Kiệt <nguyentrangiakiet47@gmail.com>
2024-02-02 22:41:24 +0700

    Using Thymeleaf to add dynamic content

--- exp_18/src/main/java/com/giakiet/easySchool/controllers/HomeController.java
+++ exp_18/src/main/java/com/giakiet/easySchool/controllers/HomeController.java
@@ -1,12 +1,14 @@
 package com.giakiet.easySchool.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping(value={"", "/", "home"})
-    public String displayHomePage() {
-         return "home.html";
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Gia Heo");
+        return "home.html";
     }
 }
